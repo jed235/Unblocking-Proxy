cea9f7fd1313682abdc2e80fa50b38574df6307f
jed235 <clayton52067@students.apsb.org>
2026-05-04 16:59:08 +0000

    yes

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const ultraviolet = require('@titaniumnetwork-dev/ultraviolet');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files
 app.use(express.static('public'));
-
-// Ultraviolet proxy middleware
-app.use('/service/', ultraviolet.middleware);
 
 // Start the server
 app.listen(PORT, () => {
